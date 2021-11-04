781611f313f882b4e6ed8e8538659800ae6ad91f
RicardoVP2002 <ricardo.vacape@gmail.com>
2021-11-04 00:40:55 -0500

    Agregando db context y string connection

---
+++ SistemaCRUD/Datos/contextoBD.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using SistemaCRUD.Models;
+
+namespace SistemaCRUD.Datos
+{
+    public class contextoBD:DbContext
+    {
+        public contextoBD(DbContextOptions<contextoBD> options):base(options)
+        {
+
+        }
+        public DbSet<Cita> citas { get; set; }
+    }
+}
