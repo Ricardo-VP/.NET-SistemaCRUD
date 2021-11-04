987e0ae2bf405b21d6ea918421025f68c1826c51
RicardoVP2002 <ricardo.vacape@gmail.com>
2021-11-04 00:31:34 -0500

    Creando el modelo Cita

---
+++ SistemaCRUD/Models/Cita.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SistemaCRUD.Models
+{
+    public class Cita
+    {
+        [Key]
+        public int id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [Display(Name = "Titulo")]
+        public string Titulo { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "Fecha")]
+        public DateTime Fecha { get; set; }
+
+        [Required]
+        [DataType(DataType.Time)]
+        [Display(Name = "Hora")]
+        public DateTime Hora { get; set; }
+    }
+}
