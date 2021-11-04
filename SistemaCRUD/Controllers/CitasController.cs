89b3cf203eee52ca306f68f841cefa22f9a48ac6
RicardoVP2002 <ricardo.vacape@gmail.com>
2021-11-04 00:57:00 -0500

    Generando controlador y vistas con scaffolding

---
+++ SistemaCRUD/Controllers/CitasController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SistemaCRUD.Datos;
+using SistemaCRUD.Models;
+
+namespace SistemaCRUD.Controllers
+{
+    public class CitasController : Controller
+    {
+        private readonly contextoBD _context;
+
+        public CitasController(contextoBD context)
+        {
+            _context = context;
+        }
+
+        // GET: Citas
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.citas.ToListAsync());
+        }
+
+        // GET: Citas/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cita = await _context.citas
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (cita == null)
+            {
+                return NotFound();
+            }
+
+            return View(cita);
+        }
+
+        // GET: Citas/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Citas/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("id,Titulo,Fecha,Hora")] Cita cita)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(cita);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(cita);
+        }
+
+        // GET: Citas/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cita = await _context.citas.FindAsync(id);
+            if (cita == null)
+            {
+                return NotFound();
+            }
+            return View(cita);
+        }
+
+        // POST: Citas/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("id,Titulo,Fecha,Hora")] Cita cita)
+        {
+            if (id != cita.id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(cita);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CitaExists(cita.id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(cita);
+        }
+
+        // GET: Citas/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cita = await _context.citas
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (cita == null)
+            {
+                return NotFound();
+            }
+
+            return View(cita);
+        }
+
+        // POST: Citas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var cita = await _context.citas.FindAsync(id);
+            _context.citas.Remove(cita);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CitaExists(int id)
+        {
+            return _context.citas.Any(e => e.id == id);
+        }
+    }
+}
